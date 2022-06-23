8a7117293de864ce179e9d40f0f3178b7ea04d7a
Karabo Ngwato <karabo@linumlabs.com>
2022-06-23 14:01:40 +0200

    deploy contract to remote network (infura)

--- hardhat-tutorial/hardhat.config.js
+++ hardhat-tutorial/hardhat.config.js
@@ -1,8 +1,15 @@
 require("@nomiclabs/hardhat-waffle");
+require('dotenv').config();
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
   solidity: "0.7.3",
+  networks: {
+    rinkeby: {
+      url: `${process.env.INFURA_RINKEBY_LINK}`,
+      accounts: [`${process.env.PRIVATE_KEY}`]
+    }
+  }
 };
