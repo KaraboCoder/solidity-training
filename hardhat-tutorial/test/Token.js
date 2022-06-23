eb466a713c5025010adaf3a0f34972eb7945dd0c
Karabo Ngwato <karabo@linumlabs.com>
2022-06-23 12:43:01 +0200

    added transactions test

--- hardhat-tutorial/test/Token.js
+++ hardhat-tutorial/test/Token.js
@@ -1,5 +1,5 @@
 const { expect } = require("chai");
-const { isCallTrace } = require("hardhat/internal/hardhat-network/stack-traces/message-trace");
+const { ethers } = require("hardhat");
 
 describe("Token contract", function () {
     it("Deployment should assign the total supply of tokens to the owner", async function () {
@@ -11,5 +11,23 @@ describe("Token contract", function () {
 
         const ownerBalance = await hardhatToken.balanceOf(owner.address);
         expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
+    });
+});
+
+describe("Transactions", function () {
+    it("Should transfer tokens between accounts", async function () {
+        const [owner, addr1, addr2] = await ethers.getSigners();
+
+        const Token = await ethers.getContractFactory("Token");
+
+        const deployedToken = await Token.deploy();
+
+        //Transfer 50 Tokens from owner to addr1
+        await deployedToken.transfer(addr1.address, 50);
+        expect(await deployedToken.balanceOf(addr1.address)).to.equal(50)
+
+        //Transfer 50 Tokens from addr1 to addr2
+        await deployedToken.connect(addr1).transfer(addr2.address, 50);
+        expect(await deployedToken.balanceOf(addr2.address)).to.equal(50);
     });
 });
