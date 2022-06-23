aa235cba08dca602be5299128658d510bade5026
Karabo Ngwato <karabo@linumlabs.com>
2022-06-23 13:11:58 +0200

    Refactored tests to be more structured

--- hardhat-tutorial/test/Token.js
+++ hardhat-tutorial/test/Token.js
@@ -1,19 +1,41 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("Token contract", function () {
-    it("Deployment should assign the total supply of tokens to the owner", async function () {
-        const [owner] = await ethers.getSigners();
+let Token;
+let deployedToken;
+let owner, addr1, addr2;
 
-        const Token = await ethers.getContractFactory("Token");
+/**
+ * Will run before each test, re-deploying the contract every time.
+**/
+beforeEach(async function () {
+    //Get contract factory and Signers
+    Token = await ethers.getContractFactory("Token");
+    [owner, addr1, addr2] = await ethers.getSigners();
 
-        const hardhatToken = await Token.deploy();
+    //Deploy contract
+    deployedToken = await Token.deploy();
+});
 
-        const ownerBalance = await hardhatToken.balanceOf(owner.address);
-        expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
+/**
+ * Test deployment of contract
+**/
+
+describe("Deployment", function () {
+    //Expect contract owner variable to be the same as signer's owner
+    it("Should set the right owner", async function () {
+        expect(await deployedToken.owner()).to.equal(owner.address);
+    });
+
+    it("Should assign the total supply of tokens to the owner", async function () {
+        const ownerBalance = await deployedToken.balanceOf(owner.address);
+        expect(await deployedToken.totalSupply()).to.equal(ownerBalance);
     });
 });
 
+/**
+ * Test Transactions
+ */
 describe("Transactions", function () {
     it("Should transfer tokens between accounts", async function () {
         const [owner, addr1, addr2] = await ethers.getSigners();
@@ -29,5 +51,15 @@ describe("Transactions", function () {
         //Transfer 50 Tokens from addr1 to addr2
         await deployedToken.connect(addr1).transfer(addr2.address, 50);
         expect(await deployedToken.balanceOf(addr2.address)).to.equal(50);
+    });
+
+    it("Should fail if sender doesn’t have enough tokens", async function () {
+        const initialOwnerBalance = await deployedToken.balanceOf(owner.address);
+
+        await expect(
+            deployedToken.connect(addr1).transfer(owner.address, 1)
+        ).to.be.revertedWith("Not enough tokens");
+
+        expect(await deployedToken.balanceOf(owner.address)).to.equal(initialOwnerBalance);
     });
 });
